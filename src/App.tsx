2fc5d32c1ba3d39adef48b158d2709813606739b
Igor Brandalise <brandaliseigor123@gmail.com>
2025-05-13 23:38:43 -0300

    estado para armazenar os dados do clima

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+import type { WeatherData} from "./types/weather";
 
+const [WeatherData, setWeatherData] = useState<WeatherData | null>(null)
 
 function App() {
   
