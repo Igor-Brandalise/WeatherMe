346c020c8248effb8dfbb8c7b4bab219cfea7fcd
Igor Brandalise <brandaliseigor123@gmail.com>
2025-05-13 23:53:51 -0300

    guardar os dados da api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
-import type { WeatherData} from "./types/weather";
-
-const [WeatherData, setWeatherData] = useState<WeatherData | null>(null)
+import { weatherByCity } from "./services/api";
+import type { WeatherData } from "./types/weather";
 
 function App() {
-  
+  const [WeatherData, setWeatherData] = useState<WeatherData | null>(null);
+
+  const [ city, setCity] = useState<string>("")
+
+  const handleSearch = async () => {
+    try {
+      const data = await weatherByCity(city);
+      setWeatherData(data);
+    } catch (error) {
+      console.error("Erro ao buscar dados do clima: ", error);
+    }
+  };
 
   return (
     <>
-      <div></div>
+      <div>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)} // Atualiza o estado de city
+          placeholder="Digite uma cidade"
+        />
+      </div>
     </>
   );
 }
