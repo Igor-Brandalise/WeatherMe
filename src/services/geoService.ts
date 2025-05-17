a88e0da8d2627be4b0893e249f55b9cd6807ab21
Igor Brandalise <brandaliseigor123@gmail.com>
2025-05-16 23:30:15 -0300

    upd

--- src/services/geoService.ts
+++ src/services/geoService.ts
@@ -4,7 +4,7 @@ export const fetchCities = async( query: string): Promise<City[]> => {
     try{
         const limit = 5
         const api_key = import.meta.env.VITE_OPENWEATHER_KEY
-        const url = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${api_key}`
+        const url = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${api_key}`;
 
         const response = await fetch(url)
         if(!response.ok){
